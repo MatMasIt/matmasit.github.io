c7adbcf9b0833e31fb4691f2d63782ddb4fe343c
Mattia Mascarello <mattia.mascarello@outlook.it>
2023-08-08 18:02:09 +0200

    Anthems, version expolorer, css filter color trick

--- script.js
+++ script.js
@@ -72,62 +72,89 @@ function specialDates() {
 
      These two things together took me 1 hour to figure out
      */
-    if (date.getMonth() + 1 == 5 && date.getDate() == 9) {
+    document.getElementById("euS").style.display = "none";
+    document.getElementById("redS").style.display = "none";
+    document.getElementById("xmaS").style.display = "none";
+    document.getElementById("natoS").style.display = "none";
+    document.getElementById("janS").style.display = "none";
+    document.getElementById("regularS").style.display = "none";
+    let force = null;
+    if (location.hash.indexOf("force:") != -1) {
+        let exp = location.hash.split(":");
+        if (exp.length == 2) force = location.hash.split(":")[1];
+        if (force == "no") force = null;
+    }
+    if ((force == null && date.getMonth() + 1 == 5 && date.getDate() == 9) || force == "eu") {
+        document.getElementById("euS").style.display = "inline";
         body.classList.add("bodyEuDay");
         portrait.src = "images/euportrait.png";
         portraitLink.href = "images/euportrait.png";
         document.querySelectorAll(".happyEuDay").forEach(function (el) {
             el.style.display = "block";
         });
-
-
+        baseColorClass = "goldColor";
+        selectedColorClass = "darkGoldColor";
     }
-    else if (date.getMonth() + 1 == 6 && date.getDate() == 2) {
+    else if ((force == null && date.getMonth() + 1 == 6 && date.getDate() == 2) || force == "it") {
+        document.getElementById("redS").style.display = "inline";
         body.classList.add("bodyRepublicDay");
         portrait.src = "images/itportrait.png";
         portraitLink.href = "images/itportrait.png";
         document.querySelectorAll(".happyRepublicDay").forEach(function (el) {
             el.style.display = "block";
         });
+        baseColorClass = "baseColor";
+        selectedColorClass = "greenColor";
     }
-    else if (date.getMonth() + 1 == 4 && date.getDate() == 4) {
+    else if ((force == null && date.getMonth() + 1 == 4 && date.getDate() == 4) || force == "nato") {
+        document.getElementById("natoS").style.display = "inline";
         body.classList.add("bodyNATODay");
         portrait.src = "images/natoportrait.png";
         portraitLink.href = "images/natoportrait.png";
         document.querySelectorAll(".happyNATODay").forEach(function (el) {
             el.style.display = "block";
         });
+        baseColorClass = "baseColor";
+        selectedColorClass = "blueColor";
     }
-    else if (date.getMonth() + 1 == 12 && date.getDate() > 18 && date.getDate() < 27) {
+    else if ((force == null && date.getMonth() + 1 == 12 && date.getDate() > 18 && date.getDate() < 27) || force == "xmas") {
+        document.getElementById("xmaS").style.display = "inline";
         body.classList.add("bodyXMAS");
         portrait.src = "images/xmasportrait.png";
         portraitLink.href = "images/xmasportrait.png";
         document.querySelectorAll(".happyXMAS").forEach(function (el) {
             el.style.display = "block";
         });
+        baseColorClass = "baseColor";
+        selectedColorClass = "redColor";
     }
-    else if (date.getMonth() + 1 == 1) { // grey january
+    else if ((force == null && date.getMonth() + 1 == 1) || force == "jan") { // grey january
+
+        document.getElementById("janS").style.display = "inline";
         body.classList.add("greyFilter");
     }
+    else {
+        document.getElementById("regularS").style.display = "inline";
+        baseColorClass = "baseColor";
+        selectedColorClass = "redColor";
+    }
+    playIcons.forEach(function (el) {
+        if (!isPlaying(musicPlayer) || playingItem != el.attributes["data-play"].textContent) {
+            el.src = playIcon;
+            setResting(el);
+        }
+        else{
+            el.src = stopIcon;
+            setSelected(el);
+        }
+    });
+    if (playing) {setSelected(pronounceImage); }
+    else {setResting(pronounceImage); setResting(mattiaText); setResting(mascarelloText); }
 }
 
 
-
-specialDates();
-setPoemOfTheDay();
-
-setInterval(function () {
-    beat();
-}, 1000);
-
-setInterval(function () {
-    specialDates();
-    setPoemOfTheDay()
-}, 5 * 60 * 1000);
-
-
-let mattiaText = document.getElementById("mattia");
-let mascarelloText = document.getElementById("mascarello");
+var mattiaText = document.getElementById("mattia");
+var mascarelloText = document.getElementById("mascarello");
 
 let mattia = new Audio("audio/mattia.ogg");
 let mascarello = new Audio("audio/mascarello.ogg");
@@ -148,36 +175,149 @@ try {
 } catch (e) { }
 
 pronounceImage.addEventListener("mouseenter", function () {
-    if (playing) return;
-    pronounceImage.src = "icons/loudspeaker-selected.svg";
+    if (playing || isPlaying(musicPlayer)) return;
+    setSelected(pronounceImage);
 });
 
 pronounceImage.addEventListener("mouseleave", function () {
-    if (playing) return;
-    pronounceImage.src = "icons/loudspeaker.svg";
+    if (playing || isPlaying(musicPlayer)) return;
+    setResting(pronounceImage);
 });
+
+
 
 mascarello.onended = function () {
     playing = false;
-    pronounceImage.src = "icons/loudspeaker.svg";
+    setResting(pronounceImage);
     setTimeout(function () {
-        mascarelloText.classList.remove("speakingNow");
+        setResting(mascarelloText)
     }, 500);
 };
 
 mattia.onended = function () {
     setTimeout(function () {
-        mattiaText.classList.remove("speakingNow");
-        mascarelloText.classList.add("speakingNow");
+        setSelected(mascarelloText)
+        setResting(mattiaText);
         mascarello.play();
     }, 1000);
 };
 pronounceLink.addEventListener("click", function () {
-    if (playing) return;
-    pronounceImage.src = "icons/loudspeaker-selected.svg";
+    if (playing || isPlaying(musicPlayer)) return;
+    setSelected(pronounceImage);
     mattia.currentTime = 0;
     mascarello.currentTime = 0;
     playing = true;
-    mattiaText.classList.add("speakingNow");
+    setSelected(mattiaText);
     mattia.play();
+});
+
+
+var musicPlayer = new Audio();
+var playingItem = "";
+
+let audioMap = {
+    nato: "audio/nato.mp3",
+    eu: "audio/eu.ogg",
+    it: "audio/ita.mp3"
+};
+
+
+var playIcon = "icons/play.svg";
+var stopIcon = "icons/stop.svg";
+var baseColorClass = "baseColor";
+var selectedColorClass = "redColor";
+
+function clearColor(target) {
+    target.classList.forEach(function (el) {
+        if (el.endsWith("Color")) {
+            target.classList.remove(el);
+        }
+    });
+}
+
+function setSelected(target) {
+    clearColor(target);
+    target.classList.add(selectedColorClass);
+}
+
+
+function setResting(target) {
+    clearColor(target);
+    target.classList.add(baseColorClass);
+}
+
+var playIcons = [];
+document.querySelectorAll(".playIcon").forEach(function (el) {
+    el.src = playIcon;
+    playIcons.push(el);
+    el.addEventListener("mouseenter", function (e) {
+        if (isPlaying(musicPlayer) && playingItem == e.target.attributes["data-play"].textContent) {
+            e.target.src = stopIcon;
+        }
+        else {
+            e.target.src = playIcon;
+        }
+        setSelected(e.target);
+    });
+    el.addEventListener("mouseleave", function (e) {
+        if (isPlaying(musicPlayer) && playingItem == e.target.attributes["data-play"].textContent) {
+            e.target.src = stopIcon;
+        }
+        else {
+            e.target.src = playIcon;
+        }
+        setResting(e.target);
+    });
+    el.parentElement.addEventListener("click", function (e) {
+        if (isPlaying(musicPlayer) && playingItem == e.target.attributes["data-play"].textContent) {
+            // stop 
+            musicPlayer.pause();
+            e.target.src = playIcon;
+            e.target.alt = "Start Playing";
+            musicPlayer.playing = false;
+            musicPlayer.currentTime = 0;
+
+        }
+        else {
+            // set all the other icons to play when we start this one
+            playIcons.forEach(function (e) {
+                e.src = playIcon;
+                e.alt = "Start Playing";
+            });
+            let tag = e.target.attributes["data-play"].textContent;
+            playingItem = tag;
+            musicPlayer.src = audioMap[tag];
+            e.target.src = stopIcon;
+            e.target.alt = "Stop Playing";
+            musicPlayer.currentTime = 0;
+            musicPlayer.play();
+        }
+    });
+});
+musicPlayer.onended = function () {
+    playIcons.forEach(function (e) {
+        e.src = playIcon;
+        e.alt = "Start Playing";
+    });
+};
+function isPlaying(myAudio) {
+    return !myAudio.paused && myAudio.currentTime > 0 && !myAudio.ended;
+}
+
+specialDates();
+setPoemOfTheDay();
+
+setInterval(function () {
+    beat();
+}, 1000);
+
+
+
+setInterval(function () {
+    specialDates();
+    setPoemOfTheDay();
+}, 5 * 60 * 1000);
+
+addEventListener("hashchange", (event) => {
+    specialDates();
 });
